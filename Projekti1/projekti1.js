8f9dc5182a60644bf9423b7293b2b884afdc6542
Jyri Lindroos <jyri.lindroos@keuda.fi>
2024-10-30 12:03:11 +0200

    Korjaukset lomakkeeseen

--- Projekti1/projekti1.js
+++ Projekti1/projekti1.js
@@ -1,13 +1,13 @@
-function laheta()
+function testi(event)
 {
+    event.preventDefault();
     //Tietojen haku taulukosta
     let sposti = document.getElementById("email").value;
     let pass = document.getElementById("password").value;
-    console.log(sposti);
     //Tietojen tarkistus lomakkeesta
-    let checkbox = document.getElementById("checkbox").isChecked;
-    let radio = document.getElementsByName("käyttäjä").isChecked;
-    let dropdown = document.getElementById("select").isChecked;
+    // let checkbox = document.getElementById("checkbox").isChecked;
+    // let radio = document.getElementsByName("käyttäjä").isChecked;
+    // let dropdown = document.getElementById("select").isChecked;
 
     //Tietojen täytön tarkistus + ilmoitus puuttuvista tiedoista
     if(sposti.trim() == "")
@@ -17,15 +17,15 @@ function laheta()
     else if(pass.trim() == "")
     {
         alert("Anna salasana!");
-        return false;
     }
 
-    else if(password.length < 8){
+    else if(pass.length < 8){
         alert("Salasanan pitää olla vähintään 8 merkkiä pitkä!")
     }
     else{
         alert("Kirjautumisesi on vastaanotettu!")
     }
 
-    event.preventDefault();
+
+    
 }
