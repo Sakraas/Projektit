6e3f47dd7c43862ca9f7bffe8781c61f66d8b32e
Juuso Sandelin <juuso95juuso@hotmail.com>
2024-10-31 10:25:02 +0200

    a

--- Projekti1/projekti1.js
+++ Projekti1/projekti1.js
@@ -2,16 +2,12 @@ function laheta(event)
 {
     event.preventDefault();
     //Tietojen haku taulukosta
-    let sposti = document.getElementById("email").value;
-    let pass = document.getElementById("password").value;
-    //Tietojen tarkistus lomakkeesta
-
-    // let checkbox = document.getElementById("checkbox").isChecked;
-    // let radio = document.getElementsByName("käyttäjä").isChecked;
-    // let dropdown = document.getElementById("select").isChecked;
+    let nimi = document.getElementById("user").value;
+    let pass = document.getElementById("pw").value;
+    let idea = document.getElementById("kehitysidea").value;
 
     //Tietojen täytön tarkistus + ilmoitus puuttuvista tiedoista
-    if(sposti.trim() == "")
+    if(nimi.trim() == "")
     {
         alert("Anna sähköposti");
     }
@@ -20,11 +16,20 @@ function laheta(event)
         alert("Anna salasana!");
     }
 
-    else if(pass.length < 8){
-        alert("Salasanan pitää olla vähintään 8 merkkiä pitkä!")
+    else if(pass.length < 6){
+        alert("Salasanan pitää olla vähintään 6 merkkiä pitkä!");
     }
+    else if(idea.trim() == "") {
+        alert("Anna jokin palaute tai -");
+    }
+
     else{
-        alert("Kirjautumisesi on vastaanotettu!")
+        alert("Kirjautumisesi on vastaanotettu!");
     }
 
 }
+
+// function reset()
+// {
+//     formElement.reset()  
+// }
