91524f0b801d634ca42dd4a8c8171bc39029e71f
Samkosken <sami.koskenranta@edu.keuda.fi>
2024-11-06 11:51:44 +0200

    muutokset html js

--- Projekti2/script.js
+++ Projekti2/script.js
@@ -7,21 +7,33 @@ let tappiot = 0;
 window.onload = window.alert(vastaus);
 
 function guessNumber(arvo) {
-    if (arvaustenMaara == 3) {
+    if (arvaustenMaara >= 3) {
         window.alert("Hävisit pelin!")
+        for (var n = 1; n <= 10; n++)
+            {
+              document.getElementById(n).classList.add("disabled");
+              document.getElementById(n).disabled = true;
+            }
+        tappiot++;
+        document.getElementById("losses").innerHTML = tappiot;
     } else 
 
         if (vastaus < arvo.value) {
-            window.alert("Arvasit liian suuren numeron")
             arvaukset.push(arvo.value);
             arvaustenMaara++
+            document.getElementById("rivi1").innerHTML = "Arvasit liian suuren numeron!";
+            document.getElementById("rivi2").innerHTML = "Arvatut numerot " + arvaukset;
+            document.getElementById("rivi3").innerHTML = "Arvausten määrä " + arvaustenMaara;
         } else if (vastaus > arvo.value) {
-            window.alert("Arvasit liian pienen numeron")
             arvaukset.push(arvo.value);
             arvaustenMaara++
+            document.getElementById("rivi1").innerHTML = "Arvasit liian pienen numeron!";
+            document.getElementById("rivi2").innerHTML = "Arvatut numerot " + arvaukset;
+            document.getElementById("rivi3").innerHTML = "Arvausten määrä " + arvaustenMaara;
     }   else {
         window.alert("Arvasit oikein!")
-        
+        voitot++;
+        document.getElementById("wins").innerHTML = voitot;
     }
 }
 window.alert(arvaus);
