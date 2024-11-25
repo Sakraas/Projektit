9511ced47e701eb05994808ec857c347747deb34
Samkosken <sami.koskenranta@edu.keuda.fi>
2024-11-25 12:07:03 +0200

    js6 kommentit

--- Projekti6/script6.js
+++ Projekti6/script6.js
@@ -1,6 +1,7 @@
 function laheta() {
 
-    const etuNimi = document.getElementById("enimi").value;
+    // Muuttujat tiedoille
+    const etuNimi = document.getElementById("enimi").value;         
     const sukuNimi = document.getElementById("snimi").value;
     const osoiteTieto = document.getElementById("osoite").value;
     const puhelinnumero = document.getElementById("puhnro").value;
@@ -8,7 +9,7 @@ function laheta() {
     const postinumero = document.getElementById("pnro").value;
     const sahkoPosti = document.getElementById("sposti").value;
 
-
+    // Luodaan objekti tiedoista
     const kayttajatiedot = {
         etunimi: etuNimi,
         sukunimi: sukuNimi,
@@ -18,13 +19,14 @@ function laheta() {
         postinumero: postinumero,
         sahkoposti: sahkoPosti,
     }
-    
+    // Muunnetaan JSON
     localStorage.setItem('user', JSON.stringify(kayttajatiedot))
 }
-
+// Tyhjennettään localStorage
 function reset() {
     localStorage.clear()
 }
+// Varmistetaan että sivu on kokonaan ladannut
 window.onload =function() {
     document.getElementById("kayttajatiedot").onsubmit = laheta
     document.getElementById("reset").onclick = reset
