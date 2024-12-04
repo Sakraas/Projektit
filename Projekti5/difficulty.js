27c35f056a5b7d5b6996d05157d9446195969f85
Juuso Sandelin <juuso95juuso@hotmail.com>
2024-12-04 12:39:46 +0200

    a

--- Projekti5/difficulty.js
+++ Projekti5/difficulty.js
@@ -1,13 +1,5 @@
 function paivitaWindowLocation() {
 
-    const vaikeus = document.getElementById("taso").value;
-
-		if (vaikeus === "easy") {
             window.location.href = "http://127.0.0.1:5500/Projekti5/level1.html";
-		} else if (vaikeus === "medium") {
-            window.location.href = "http://127.0.0.1:5500/Projekti5/level2.html"
-		} else if (vaikeus === "hard") {
-            window.location.href = "http://127.0.0.1:5500/Projekti5/level3.html"
-		}
-            peli()
+
 }
--- Projekti5/game1111111.js
+++ Projekti5/game1111111.js
@@ -95,17 +95,17 @@
 //     ];
 
 
-function paivitaWindowLocation() {
+// function paivitaWindowLocation() {
 
-    const vaikeus = document.getElementById("taso").value;
-		if (vaikeus === "easy") {
-            window.location.href = "http://127.0.0.1:5500/Projekti5/level1.html";
-		} else if (vaikeus === "medium") {
-            window.location.href = "http://127.0.0.1:5500/Projekti5/level2.html"
-		} else if (vaikeus === "hard") {
-            window.location.href = "http://127.0.0.1:5500/Projekti5/level3.html"
-        }
-}
+//     const vaikeus = document.getElementById("taso").value;
+// 		if (vaikeus === "easy") {
+//             window.location.href = "http://127.0.0.1:5500/Projekti5/level1.html";
+// 		} else if (vaikeus === "medium") {
+//             window.location.href = "http://127.0.0.1:5500/Projekti5/level2.html"
+// 		} else if (vaikeus === "hard") {
+//             window.location.href = "http://127.0.0.1:5500/Projekti5/level3.html"
+//         }
+// }
 
 
 
@@ -149,7 +149,7 @@ document.addEventListener("DOMContentLoaded", ()  => {
     }
 
 function peli(){
-    const vaikeus = document.getElementById("taso").value;
+    vaikeus = document.getElementById("taso").value;
     console.log(vaikeus)
     if(vaikeus === "easy") {
         kortit = pienipeli;
@@ -161,7 +161,7 @@ function peli(){
     }
     console.log(kortit);
 }
-peli();
+
 console.log(kortit);
     
 // Korttien sekoitus
