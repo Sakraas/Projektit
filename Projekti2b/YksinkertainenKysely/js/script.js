691bd041734db2679a7f24f73170126c15185aab
Juuso Sandelin <juuso95juuso@hotmail.com>
2024-12-11 11:08:18 +0200

    documentaatioita

--- Projekti2b/YksinkertainenKysely/js/script.js
+++ Projekti2b/YksinkertainenKysely/js/script.js
@@ -1,43 +1,29 @@
 function lahetaVastaukset(event) {
-    
-}
+    event.preventDefault(); // Estää oletustoiminnon (lomakkeen lähetyksen)
 
-let yhteensa = 5;
-let pisteet = 0;
-let huom = 0;
+    const yhteensa = 5; // Kysymysten kokonaismäärä
+    let pisteet = 0;
+    let huom = false;
 
-//hakee käyttäjän syöttämät vastaukset
-let k1 = document.forms["kyselyLomake"]["k1"].value;
-let k2 = document.forms["kyselyLomake"]["k2"].value;
-let k3 = document.forms["kyselyLomake"]["k3"].value;
-let k4 = document.forms["kyselyLomake"]["k4"].value;
-let k5 = document.forms["kyselyLomake"]["k5"].value;
+    // Käyttäjän syöttämien vastausten haku
+    const lomake = document.forms["kyselyLomake"];
+    let vastaukset = ["b", "a", "c", "b", "d"]; // Oikeat vastaukset
+    let tulokset = document.getElementById("tulokset");
 
-//Lomakkeen tarkistus
-for(i = 1; i <= yhteensa; i++)
-{
-    if(eval('k'+i) == null || eval('k'+i) == '')
-    {
-        alert('Et vastannut kysymykseen numero: ' + i);
-        huom = 1;
+    // Tarkista vastaukset
+    for (let i = 1; i <= yhteensa; i++) {
+        let vastaus = lomake[`k${i}`].value;
+        if (!vastaus) {
+            alert(`Et vastannut kysymykseen numero: ${i}`);
+            huom = true;
+        } else if (vastaus === vastaukset[i - 1]) {
+            pisteet++; // Kasvata pistemäärää oikeista vastauksista
+        }
     }
-    if(huom == 1) return false;
+
+    if (huom) return; // Lopeta, jos jokin kysymys jäi vastaamatta
+
+    // Näytä tulokset
+    alert(`Sait ${pisteet} pistettä, kun maksimi pistemäärä oli ${yhteensa}`);
+    tulokset.innerHTML = `<h3>Sait <span>${pisteet}</span> pistettä, kun maksimi pistemäärä oli <span>${yhteensa}</span></h3>`;
 }
-
-//Oikeat vastaukset
-let vastaukset = ["b", "a", "d", "b", "d"];
-
-//Oikeiden vastauksien tarkistus
-for(i = 1; i <= yhteensa; i++)
-{
-    if(eval('k'+i) == vastaukset[i-1])
-    {
-        pisteet++;
-    }
-}
-
-//Tuloksien tulostus
-let tulokset = document.getElementById('tulokset');
-alert('Sait ' +pisteet + ' pistettä, kun maksimi pistemäärä oli ' + yhteensa);
-tulokset.innerHTML = '<h3> Sait <span>' + pisteet + '</span> pistettä, kun maksimi pistemäärä oli <span>' +yhteensa + '</span></h3>';
-return false;
