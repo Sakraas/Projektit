b62d29cadb5fec7179a47011e7e25c320a0a6fe1
Samkosken <sami.koskenranta@edu.keuda.fi>
2024-12-11 13:00:57 +0200

    JS kommentit

--- Projekti10/script.js
+++ Projekti10/script.js
@@ -1,24 +1,30 @@
+// Odottaa, että sivu latautuu kokonaan ennen scriptin suorittamista
 document.addEventListener("DOMContentLoaded", () => {
-    const valinnat = document.querySelectorAll(".valinta");
-    const tulosDiv = document.getElementById("tulos")
-    const vaihtoehdot = ["kivi", "paperi", "sakset"];
+    const valinnat = document.querySelectorAll(".valinta"); // Muuttuja valitsemaan kaikki valinta napit
+    const tulosDiv = document.getElementById("tulos")   // Muuttuja tuloksen näyttämiseen
+    const vaihtoehdot = ["kivi", "paperi", "sakset"];   // Taulukko mahdollisista valinnoista
     
+    // Functio tietokoneen valinnalle
+    // Satunnainen indexi numero generoidaan vaihtoehto taulukosta ja palautetaan se indexi.
     function tietokoneenValinta() {
         const satunnainenIndexi = Math.floor(Math.random() * vaihtoehdot.length);
         return vaihtoehdot[satunnainenIndexi];
     }
     
     
-
+    // Lisää klikkaustapahtuman kuuntelija jokaiselle valintanapille
     valinnat.forEach((valinta) => {
         valinta.addEventListener("click", () => {
-          const pelaajaValinta = valinta.getAttribute("data-valinta");
-          const tietokoneValinta = tietokoneenValinta();
-          const tulos = maaritaVoittaja(pelaajaValinta, tietokoneValinta);
-          displayResult(pelaajaValinta, tietokoneValinta, tulos);
+          const pelaajaValinta = valinta.getAttribute("data-valinta"); // Haetaan käyytäjän valinta 
+          const tietokoneValinta = tietokoneenValinta();               // Haetaan tietokoneen satunnainen valinta
+          const tulos = maaritaVoittaja(pelaajaValinta, tietokoneValinta); // Määrittää voittajan
+          displayResult(pelaajaValinta, tietokoneValinta, tulos);   // Näytetään pelin tulos div näytöllä
         });
       });
 
+    // Functio pelin voittajan määrittämiseksi
+    // pelaajaValinta - Käyttäjän valinta (kivi, paperi tai sakset)
+    // tietokoneValinta - Tietokoneen valinta (kivi, paperi tai sakset)  
     function maaritaVoittaja(pelaajaValinta, tietokoneValinta) {
         if (pelaajaValinta === tietokoneValinta) {
             return "Tasapeli!"
@@ -34,12 +40,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
          
     }
-    /**
-     * Funktio näyttää pelin tuloksen resultDiv-elementissä.
-     * @param {string} userChoice - Käyttäjän valinta.
-     * @param {string} computerChoice - Tietokoneen valinta.
-     * @param {string} result - Pelin tulos.
-     */
+    // Functio näyttämään pelin tulosDiv näytössä
     function displayResult(pelaajaValinta, tietokoneValinta, tulos) {
         tulosDiv.innerHTML = `
         Sinä valitsit: ${pelaajaValinta}<br>
